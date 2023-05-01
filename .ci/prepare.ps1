a1f99d6da13c2be4fd782d1a17c7ca9d940cfe9f
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2023-05-01 14:47:23 +0200

    Bump to .NET 7
    And update CI flow to support it

--- .ci/prepare.ps1
+++ .ci/prepare.ps1
@@ -1,8 +1,10 @@
 if ($env:_BUILD_BRANCH -eq "refs/heads/master" -Or $env:_BUILD_BRANCH -eq "refs/tags/canary") {
   $env:_IS_BUILD_CANARY = "true"
+  $env:_IS_GITHUB_RELEASE = "true"
 }
 elseif ($env:_BUILD_BRANCH -like "refs/tags/*") {
   $env:_BUILD_VERSION = $env:_BUILD_VERSION.Substring(0,$env:_BUILD_VERSION.LastIndexOf('.')) + ".0"
+  $env:_IS_GITHUB_RELEASE = "true"
 }
 $env:_RELEASE_VERSION = "v${env:_BUILD_VERSION}"
 
@@ -14,3 +16,4 @@ Write-Output "--------------------------------------------------"
 Write-Host "##vso[task.setvariable variable=_BUILD_VERSION;]${env:_BUILD_VERSION}"
 Write-Host "##vso[task.setvariable variable=_RELEASE_VERSION;]${env:_RELEASE_VERSION}"
 Write-Host "##vso[task.setvariable variable=_IS_BUILD_CANARY;]${env:_IS_BUILD_CANARY}"
+Write-Host "##vso[task.setvariable variable=_IS_GITHUB_RELEASE;]${env:_IS_GITHUB_RELEASE}"
